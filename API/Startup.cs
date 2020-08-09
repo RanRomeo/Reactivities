46728c658214cdb5652fdd3a32baf9783cbbff98
Ran <ranzor007@outlook.com>
2020-08-09 17:22:29 +0530

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,8 +34,13 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
 
-
-
+            services.AddCors(opt =>
+             {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
            
         }
@@ -53,7 +58,7 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
